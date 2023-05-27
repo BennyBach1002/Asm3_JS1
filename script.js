f5695536598e1ee3ce92ab737526b7b92cbffd66
Ben White <benbach2001@gmail.com>
2023-05-27 18:43:07 +0700

    update

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 
 const computerPlay = () => {
-    const arr = ['rock', 'paper', 'scissors'];
+    const arr = ['rock', 'paper', 'scissor'];
     const randomIndex = Math.floor(Math.random() * arr.length)
     const item = arr[randomIndex]
     return item
 }
-const botSelection = computerPlay()
 
 const playRound = (playerSelection, botSelection) => {
+    botSelection = computerPlay()
     playerSelection = playerSelection.toLowerCase()
     if(playerSelection === botSelection){
         return 'Tie'
     }
-    else if(playerSelection == 'rock'){
-        if(botSelection == 'paper'){
-            return 'Computer Won';
+    // else if(playerSelection == 'rock'){
+    //     return `rock vs ${botSelection}`
+    // }
+    // else if(playerSelection == 'paper'){
+    //     return `paper vs ${botSelection}`
+    // }
+    else if(playerSelection === 'rock'){
+        if(botSelection === 'paper'){
+            return 'You lose! rock lost to paper!';
         }else{
-            return 'Player Won'
+            return 'You won! rock beat scissor!';
         }
     }
-    else if(playerSelection == 'scissors'){
+    else if(playerSelection == 'scissor'){
         if(botSelection == 'rock'){
-            return 'Computer Won';
+            return `You lose! scissor lost to ${botSelection}! `;
         }else{
-            return 'Player Won';
+            return 'You won! scissor beats paper!';;
         }
     }
     else if(playerSelection == 'paper'){
-        if(botSelection == 'scissors'){
-            return 'Computer Won';
+        if(botSelection == 'scissor'){
+            return 'You lose! paper lost to scissor!';
         }else{
-            return 'Player Won';
+            return 'You won! paper beats rock!';;
         }
     }
     let Result = `You Lose! ${playerSelection} beat ${botSelection}`
     return Result
 }
-
-console.log();
